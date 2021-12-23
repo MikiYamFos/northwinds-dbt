557ef48bfaf0250d40220e80027adf83cb34e780
miki <miki.faux.foster@gmail.com>
2021-12-23 00:05:09 +0000

    building customer staging table

---
+++ models/staging/stg_rds_customers.sql
@@ -0,0 +1,10 @@
+WITH source as (
+    SELECT * FROM "FIVETRAN_DATABASE"."NORTHWINDS_RDS_PUBLIC"."CUSTOMERS" ),
+
+renamed as (
+SELECT customer_id, country,
+    SPLIT_PART(contact_name, ' ', 1) as first_name,
+    SPLIT_PART(contact_name), ' ', -1) as last_name
+    FROM source
+)
+SELECT * FROM renamed
