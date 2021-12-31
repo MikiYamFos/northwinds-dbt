6094a4ea9caeef9f1fca181ed3ef4c37812b0fde
miki <miki.faux.foster@gmail.com>
2021-12-31 00:15:57 +0000

    integration layer

--- models/integration/int_companies.sql
+++ models/integration/int_companies.sql
@@ -1,7 +1,18 @@
-WITH source as (
+with hubspot_company as (SELECT * 
+FROM {{ ref('stg_hubspot_companies') }}
+),
+rds_company as (
     SELECT * FROM {{ ref('stg_rds_companies') }}
 ),
-company_join as (
-     SELECT * FROM {{ ref('stg_rds_companies') }}
+comp_joined as (
+    SELECT company_id as hubspot_id, null as rds_id, business_name from hubspot_company union all
+    SELECT null as hubspot_id, company_id as rds_id, company_name as business_name from rds_company
+),
+deduped as (
+    SELECT {{ dbt_utils.surrogate_key(['business_name']) }} as contact_pk,
+    max(hubspot_id), max(rds_id), business_name
+    FROM comp_joined
+    GROUP BY business_name
+    ORDER BY business_name
 )
-SELECT * FROM company_join
+SELECT * from deduped
